d134b2b86e53f1b4aa8bf7bc608096b537066aa8
DerFredy - @4llfr33d0m:matrix.org <derfredy@gmail.com>
2019-06-26 10:04:56 +0200

    Correccion de errores

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 
+sleep 5
+mkdir -p /root/.gaiad/config
+
 if [ ! -f "/root/.gaiad/config/node_key.json" ]
 then
-  gaiad init Cosmos-DAppNode
-  cp /genesis.json /root/.gaiad/config/
-  cp /config.toml  /root/.gaiad/config/
+  cp /genesis.json /root/.gaiad/config
+  cp /config.toml  /root/.gaiad/config
 fi
 
 if [ ! -f "/root/.gaiad/config/genesis.json" ]
 then
-  cp /genesis.json /root/.gaiad/config/
+  cp /genesis.json /root/.gaiad/config/genesis.json
 fi
 
 if [ ! -f "/root/.gaiad/config/config.toml" ]
 then
-  cp /config.toml /root/.gaiad/config/
+  cp /config.toml /root/.gaiad/config/config.toml
 fi
 
 /root/start_script.sh
