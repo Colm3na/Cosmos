1843561597d36ffce0ea825a22c0d6b64cc3e8ef
DerFredy - @4llfr33d0m:matrix.org <derfredy@gmail.com>
2019-07-17 13:51:06 +0200

    Cambiado matrix user

--- docker/start_script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Start the first process
-tmux new-session -s "gaiad" -d -n "main"
-tmux send-keys -t "gaiad:main" C-z 'gaiad start' Enter
-tmux split-window -v
-tmux select-pane -t 1
-tmux send-keys "cd /usr/src/app/lunie && npm run serve" C-m
